24f1140ac97e99c47698c55ece9b078c6fd5eff2
Natalia Ignateva <ignatyevanat@gmail.com>
2021-06-24 11:42:36 +0200

    Gather data from Github

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+import axios from 'axios';
+
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('form')
-  form.addEventListener('submit', (event) => {
-    event.preventDefault()
-    const username = document.querySelector('input').value
-    alert(username)
-  })
-})
+  const form = document.querySelector('form');
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const username = document.querySelector('input').value;
+
+    const response = await axios.get(
+      `https://api.github.com/users/${username}`,
+    );
+    console.log(response.data);
+  });
+});
